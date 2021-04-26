df0f3ad353fcffc1a1d8e99ff74667adf5df2cc1
Dan Sinclair <dan.sinclair@poqcommerce.com>
2021-04-26 09:34:51 +0100

    Add daily profit calculation

--- miners.py
+++ miners.py
@@ -27,6 +27,11 @@ def main():
         username = input('Enter your DUCO username: ')
 
     print("Fetching miners list…")
+
+    prev_balance = 0
+
+    last_update = None
+
     while True:
         try:
             miners_response = requests.get("https://server.duinocoin.com/miners.json")
@@ -36,6 +41,10 @@ def main():
             balances_response = requests.get("https://server.duinocoin.com/balances.json")
             balances_response.raise_for_status()
             balances_json_data = json.loads(balances_response.text)
+
+            api_response = requests.get("https://server.duinocoin.com/api.json")
+            api_response.raise_for_status()
+            api_json_data = json.loads(api_response.text)
 
         except HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
@@ -49,8 +58,10 @@ def main():
             print('Exiting…')
             sys.exit()
         
-        if miners_json_data and balances_json_data:
+        if miners_json_data and balances_json_data and api_json_data:
             subprocess.call("clear")
+
+            duco_price_usd = api_json_data.get('Duco price', 0)
 
             user_miners = [v for v in miners_json_data.values() if v["User"] == username]
 
@@ -88,14 +99,23 @@ def main():
             
             miners.sort(key=itemgetter(0))
 
-            user_balance = balances_json_data.get(username, "- DUCO")
+            user_balance_str = balances_json_data.get(username, "0 DUCO").replace(' DUCO', ' ᕲ')
+            user_balance = float(user_balance_str.replace(' ᕲ', ''))
+
+            balance_difference = user_balance - prev_balance
+            time_difference = time.time() - last_update if last_update else 0
+            daily_average = balance_difference*float((60/time_difference)*60*24) if time_difference != 0 else 0
+
             total_success_pc = int((totalAccepted/totalSharerate)*100) if totalSharerate > 0 else 0
             total_success = f'{total_success_pc}% ({totalAccepted}/{totalSharerate})'
-            print(tabulate([[user_balance, len(miners), format_hashrate(total_hash), f'{total_success}']], headers=["Balance", "Total miners", "Total hashrate", "Total success"]))
+            print(tabulate([[user_balance_str, len(miners), format_hashrate(total_hash), f'{total_success}', f'{daily_average} ᕲ']], headers=["Balance", "Total miners", "Total hashrate", "Total success", "Daily profit"]))
             
             if miners:
                 print(tabulate(miners, headers=["ID", "Software", "Algo", "Success", "Hashrate", "Diff"], tablefmt='fancy_grid'))
             
+            prev_balance = user_balance
+            last_update = time.time()
+
             time.sleep(15)
 
 if __name__ == "__main__":
